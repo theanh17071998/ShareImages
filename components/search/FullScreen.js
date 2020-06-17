aa86cf22b48e606f34da8a7b1fafc075c49375e8
theanh17071998 <nguyentheanh17071998@gmail.com>
2020-06-17 22:44:06 +0700

    add avatar of image detail, input comment

--- components/search/FullScreen.js
+++ components/search/FullScreen.js
@@ -3,13 +3,13 @@ import {
     View,
     Text,
     StyleSheet,
-    Image,
+  
     Dimensions
 } from 'react-native'
 import { Card, Header} from 'react-native-elements'
 import ImageViewer from 'react-native-image-zoom-viewer';
 import { ScrollView } from 'react-native-gesture-handler';
-import { Icon, Button } from 'react-native-elements'
+import { Icon, Button, ListItem, Image, Input } from 'react-native-elements'
 import Comments from '../search/comment'
 
 import image from '../../assets/b72e2df60fbc06a54ff1e98cc79a1f7c.jpg'
@@ -26,6 +26,14 @@ function FullScreen(props) {
         props.clickBack()
     }
 
+    const list = [
+        {
+          name: 'Amy Farha',
+          avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+          subtitle: 'Vice President'
+        }
+      ]
+       
     return (
         <View style={styles.container}>
             <Header
@@ -36,17 +44,26 @@ function FullScreen(props) {
                 
             />
             <View>
+ 
                 <ScrollView style={styles.scrollView}>
                  <Card  style={styles.card}
                       image={image}
                       imageStyle= {styles.images}
-                      >
+                      > 
+                        <View>
+                            {
+                                <ListItem
+                                  leftAvatar={{ source: { uri: list[0].avatar_url } }}
+                                  title={list[0].name}
+                                />
+                            }
+                        </View>
                       <Text style={{marginBottom: 10}}>
                       "Tuổi trẻ đứa nào cũng quan điểm lắm , rồi sóng gió tới tát cho lệch mồm mới hiểu được..." ------ Tác giả : Rose ( Pink )
                       </Text>
                       <View style={styles.btn}>
                       <Button
-                        title='Truy cập' 
+                        title='Like' 
                         buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0, width: screenWidth(30),  backgroundColor:'red', borderRadius:5}}
                         />
                         
@@ -55,6 +72,11 @@ function FullScreen(props) {
                         title='Lưu' />
                       </View>
                     </Card>
+                    <Input
+                        placeholder='Enter your comment'
+                        inputContainerStyle={{marginHorizontal: screenWidth(2), paddingLeft: screenWidth(1), marginTop: screenHeight(2)}}
+                        rightIcon={{ type: 'font-awesome', name: 'paper-plane'}}
+                    />
                     <Comments/>
                 </ScrollView>
             </View>
