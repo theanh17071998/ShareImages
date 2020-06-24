57453755d46d079104ab198ae2f75b8083cd1842
Nguyen Ngoc Hai <ngochai220998@gmail.com>
2020-06-24 16:00:31 +0700

    update

--- constants/config.js
+++ constants/config.js
@@ -1,17 +1,17 @@
-export default {
-  dev: {
-    BACKEND_ADDRESS: 'http://192.168.43.171:3000',
-    SOCKET_SERVER: 'http://192.168.43.171:4000'
-  },
-  prod: {
-    BACKEND_ADDRESS: 'https://uet-japit-bot-discord.herokuapp.com',
-    SOCKET_SERVER: 'https://uet-japit-bot-discord.herokuapp.com'
-  },
-  local: {
-    BACKEND_ADDRESS: 'http://192.168.43.171:3000',
-    SOCKET_SERVER: 'http://192.168.43.171:4000'
-  }
-}
+// export default {
+//   dev: {
+//     BACKEND_ADDRESS: 'http://192.168.43.171:3000',
+//     SOCKET_SERVER: 'http://192.168.43.171:4000'
+//   },
+//   prod: {
+//     BACKEND_ADDRESS: 'https://uet-japit-bot-discord.herokuapp.com',
+//     SOCKET_SERVER: 'https://uet-japit-bot-discord.herokuapp.com'
+//   },
+//   local: {
+//     BACKEND_ADDRESS: 'http://192.168.43.171:3000',
+//     SOCKET_SERVER: 'http://192.168.43.171:4000'
+//   }
+// }
 
 // export default {
 //   dev: {
@@ -27,3 +27,20 @@ export default {
 //     SOCKET_SERVER: 'http://192.168.23.102:4000'
 //   }
 // }
+
+
+
+export default {
+  dev: {
+    BACKEND_ADDRESS: 'https://uet-japit-bot-discord.herokuapp.com',
+    SOCKET_SERVER: 'https://uet-japit-bot-discord.herokuapp.com'
+  },
+  prod: {
+    BACKEND_ADDRESS: 'https://uet-japit-bot-discord.herokuapp.com',
+    SOCKET_SERVER: 'https://uet-japit-bot-discord.herokuapp.com'
+  },
+  local: {
+    BACKEND_ADDRESS: 'https://uet-japit-bot-discord.herokuapp.com',
+    SOCKET_SERVER: 'https://uet-japit-bot-discord.herokuapp.com'
+  }
+}
